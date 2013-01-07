85535f85dbe3aa8e56037f3645e126ce120007d7
jeando <favreau@polytech.unice.fr>
2013-01-07 13:53:49 +0100

    traitement

---
+++ src/traitement.h
@@ -0,0 +1,13 @@
+#ifndef TRAITEMENT_H
+#define TRAITEMENT_H
+
+#include<vector>
+
+#include"ft.h"
+
+std::vector<double> autocorrelation(std::vector double input);
+std::vector<double> decoupage(std::vector double input);
+std::vector<std::vector<double> > spectrogramme(std::vector<double> input);
+
+#endif
+
