5bc8fae85370f744f2337c6bc33804f62c75f74e
guenon <guenon@polytech.unice.fr>
2013-01-18 14:27:41 +0100

    draw

--- src/Game/Menu.cpp
+++ src/Game/Menu.cpp
@@ -6,11 +6,18 @@
 using namespace std;
 
 Menu::Menu(SDL_Surface* scre)
-:alsc(), game(scre, &alsc), screen(scre)
+:alsc(), game(scre, &alsc), screen(scre), fene_menu(SDL_CreateRGBSurface(scre->flags, scre->w,
+    scre->h, scre->format->BitsPerPixel, scre->format->Rmask, scre->format->Gmask, scre->format->Bmask, scre->format->Amask))
+{
+    draw();
+    update();
+}
+
+void Menu::draw()
 {
     SDL_WM_SetCaption("Menu", nullptr);
     TTF_Init();
-    SDL_FillRect(screen, nullptr, SDL_MapRGB(screen->format, 17, 206, 112));
+    SDL_FillRect(fene_menu, nullptr, SDL_MapRGB(fene_menu->format, 17, 206, 112));
 
     SDL_Surface* commencer = IMG_Load( "../../images/buttun_begin.png" );
     SDL_Surface* standard = IMG_Load( "../../images/buttun.png" );
@@ -22,42 +29,47 @@ Menu::Menu(SDL_Surface* scre)
     //positionnement et affichage des boutons
     rect1.x=620;
     rect1.y=20;
-    SDL_BlitSurface(commencer,nullptr,screen,&rect1);
+    SDL_BlitSurface(commencer,nullptr,fene_menu,&rect1);
     ostringstream oss;
     oss << " Commencer";
     SDL_Surface* texte = TTF_RenderText_Blended(police, oss.str().c_str(), couleur);
-    SDL_BlitSurface(texte,nullptr,screen,&rect1);
+    SDL_BlitSurface(texte,nullptr,fene_menu,&rect1);
 
     rect2.x=400;
     rect2.y=350;
-    SDL_BlitSurface(standard,nullptr,screen,&rect2);
+    SDL_BlitSurface(standard,nullptr,fene_menu,&rect2);
     ostringstream oss2;
     oss2 << "   Utilisateur";
     texte = TTF_RenderText_Blended(police, oss2.str().c_str(), couleur);
-    SDL_BlitSurface(texte,nullptr,screen,&rect2);
+    SDL_BlitSurface(texte,nullptr,fene_menu,&rect2);
 
     rect3.x=900;
     rect3.y=350;
-    SDL_BlitSurface(standard,nullptr,screen,&rect3);
+    SDL_BlitSurface(standard,nullptr,fene_menu,&rect3);
     ostringstream oss3;
     oss3 << "    Options";
     texte = TTF_RenderText_Blended(police, oss3.str().c_str(), couleur);
-    SDL_BlitSurface(texte,nullptr,screen,&rect3);
+    SDL_BlitSurface(texte,nullptr,fene_menu,&rect3);
 
     rect4.x=620;
     rect4.y=700;
-    SDL_BlitSurface(quitter,nullptr,screen,&rect4);
+    SDL_BlitSurface(quitter,nullptr,fene_menu,&rect4);
     ostringstream oss4;
     oss4 << "     Quitter";
     texte = TTF_RenderText_Blended(police, oss4.str().c_str(), couleur);
-    SDL_BlitSurface(texte,nullptr,screen,&rect4);
+    SDL_BlitSurface(texte,nullptr,fene_menu,&rect4);
 
-    SDL_Flip(screen);
     SDL_FreeSurface(commencer);
     SDL_FreeSurface(texte);
     SDL_FreeSurface(standard);
     SDL_FreeSurface(quitter);
     TTF_Quit();
+}
+
+void Menu::update()
+{
+    SDL_BlitSurface(fene_menu,nullptr,screen,nullptr);
+    SDL_Flip(screen);
 }
 
 void Menu::start()
@@ -87,7 +99,7 @@ bool Menu::gestion_clic()
                         cout << "commencer" << endl;
                         Game g(screen,&alsc);
                         g.start();
-                        return true;
+
                 }
                 else if(x>=rect2.x && x<=(rect2.x+204)
                    && y>=rect2.y && y<=(rect2.y+43))
@@ -114,6 +126,7 @@ bool Menu::gestion_clic()
             default:
                 break;
         }
+        update();
     }
     return false;
 }
