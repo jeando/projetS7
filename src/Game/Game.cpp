471df4d6ad8134eaf593810ae61d74eb5044fa0e
Jean-Paul Achard <jachard@x138.(none)>
2013-01-22 16:07:37 +0100

    maj

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -36,26 +36,18 @@ bool Game::gestionSDL_Event()
                         return false;
                         break;
                     case SDLK_z:
-                        map.change_speed(0,-1);
-                        break;
                     case SDLK_UP:
                         map.change_speed(0,-1);
                         break;
                     case SDLK_q:
-                        map.change_speed(-1,0);
-                        break;
                     case SDLK_LEFT:
                         map.change_speed(-1,0);
                         break;
                     case SDLK_s:
-                        map.change_speed(0,1);
-                        break;
                     case SDLK_DOWN:
                         map.change_speed(0,1);
                         break;
                     case SDLK_d:
-                        map.change_speed(1,0);
-                        break;
                     case SDLK_RIGHT:
                         map.change_speed(1,0);
                         break;
