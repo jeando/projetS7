03e255cd240a67efa1ea7aef730c4e76a0822e42
jeando <favreau@polytech.unice.fr>
2013-01-22 17:52:54 +0100

    a

---
+++ src/test/dtw3.cpp
@@ -0,0 +1,137 @@
+#include"../MyAL.h"
+#include"../traitement.h"
+#include"../sdl.h"
+#include<iostream>
+int main()
+{
+	std::vector<ALshort> samples;
+	std::vector<std::pair<std::vector<ALshort>,SF_INFO> >samples_ref;
+	samples_ref.push_back(MyAL::load_sound("gauche_ref.wav"));
+	samples_ref.push_back(MyAL::load_sound("droite_ref.wav"));
+	samples_ref.push_back(MyAL::load_sound("haut_ref.wav"));
+	samples_ref.push_back(MyAL::load_sound("bas_ref.wav"));
+	std::string continuer("go");
+    std::chrono::milliseconds dura(1000);
+	AL_Capture alc(MyAL::choisir_device(),MyAL::choisir_capture_device());
+	std::vector<std::vector<double> > spectro;
+	unsigned int debut,size;
+	std::vector<std::vector<double> > spectro2;
+	std::vector<std::vector<double> > spectro_temp;
+	std::vector<double> vdist;
+	std::vector<double> vdist2;
+	std::vector<double> vdist3;
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Surface* screen(nullptr);
+	screen = SDL_SetVideoMode(1,1,32,
+			 SDL_HWSURFACE | SDL_DOUBLEBUF);
+	int min;
+	int min_indice;
+	int aaa;
+	while(continuer!="q"&&continuer!="quit"
+		&&continuer!="e"&&continuer!="exit"
+		&&continuer!=":q"){
+		samples.clear();
+		vdist.clear();
+		vdist2.clear();
+		vdist3.clear();
+	std::cout << "speak" << std::endl;
+	alc.start(samples);
+    std::this_thread::sleep_for(dura);
+    alc.stop();
+	std::cout << "end speak" << std::endl;
+
+	alc.save_sound("test.wav");
+	
+	spectro2 =	spectro =  equalize_spectrogramme(spectrogramme(
+								MyAL::sample_to_double(samples),
+								alc.getSampleRate()));
+	debut = indice_debut(spectro2);
+	if(debut == -1){
+		std::cout << "parler plus fort" << std::endl;
+		std::cin >> continuer;
+		continue;
+	}
+	size = spectro2[0].size();
+	for(unsigned int i = 0; i<debut; i++)
+	{
+		for(unsigned int j=0; j<size; j++)
+		{
+			spectro[i].push_back(0);
+		}
+	}
+	for(unsigned int i = debut; i<spectro.size(); i++)
+	{
+		for(unsigned int j=0; j<size; j++)
+		{
+			spectro[i].push_back(spectro2[i][j]);
+		}
+	}
+	std::cout << "start analyse" << std::endl;
+	for(auto a : samples_ref){
+		spectro_temp = equalize_spectrogramme(spectrogramme(MyAL::sample_to_double(a.first), a.second.samplerate));
+		debut = indice_debut(spectro_temp);
+		size = spectro_temp[0].size();
+		for(unsigned int i = 0; i<debut; i++)
+		{
+			for(unsigned int j=0; j<size; j++)
+			{
+				spectro[i].push_back(0);
+			}
+		}
+		for(unsigned int i = debut; i<spectro.size(); i++)
+		{
+			for(unsigned int j=0; j<size; j++)
+			{
+				spectro[i].push_back(spectro_temp[i][j]);
+			}
+		}
+		vdist.push_back(distance(dynamic_time_warping(spectro2,spectro_temp,3)));
+		vdist2.push_back(distance(dynamic_time_warping2(spectro2,spectro_temp,3)));
+		std::vector<double> temp=get_min_dist_spectre(spectro2,spectro_temp,3,0,0,spectro2.size());
+		vdist3.push_back(*std::min_element(temp.begin(), temp.end()));
+	}
+	draw_mat(screen, spectro,8,4);
+	aaa=0;
+aaa_aaa:
+	min=vdist[0];
+	min_indice=0;
+	for(unsigned int i=0; i<vdist.size(); i++)
+	{
+		std::cout << vdist[i] << std::endl;
+		if(vdist[i]<min){
+			min=vdist[i];
+			min_indice=i;
+		}
+	}
+	switch(min_indice)
+	{
+		case 0:
+			std::cout << "gauche" << std::endl;
+			break;
+		case 1:
+			std::cout << "droite" << std::endl;
+			break;
+		case 2:
+			std::cout << "haut" << std::endl;
+			break;
+		case 3:
+			std::cout << "bas" << std::endl;
+			break;
+	}
+	if(aaa==0)
+	{
+		aaa++;
+		vdist=vdist2;
+		goto aaa_aaa;
+	}
+	else if(aaa==1)
+	{
+		aaa++;
+		vdist=vdist3;
+		goto aaa_aaa;
+	}
+	std::cin >> continuer;
+	}
+	SDL_SaveBMP(screen, "test_detect_debut.bmp");
+	return EXIT_SUCCESS;
+}
